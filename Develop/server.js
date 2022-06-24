aa5f5b6d4baa80641f18e09b9f17e7cd6d655135
Clayton S <desertcow07@gmail.com>
2022-06-23 20:34:18 -0700

    Enabled ENV file and DB connection

--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
+const sequelize = require('./config/connection');
 // import sequelize connection
 
 const app = express();
@@ -8,9 +9,14 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Enable Routes
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+// Enable connection to Remote DB and Start Local server for API to connect
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`\n\x1b[42m  ~~~ Server Start Success! ~~~  \x1b[0m`);
+    console.log(`\x1b[43m    http://localhost:${PORT}/api/   \x1b[0m`);
+    //TODO Add print out of API commands/paths
+  });
 });
