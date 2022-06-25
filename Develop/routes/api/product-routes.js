ad85329b12e5d1b1cb93c9e782069d411c361cfd
Clayton S <desertcow07@gmail.com>
2022-06-25 14:35:43 -0700

    Enabled API Delete oin CAT/Tags/Products

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -103,15 +103,33 @@ router.put('/:id', (req, res) => {
         ProductTag.bulkCreate(newProductTags),
       ]);
     })
-    .then((updatedProductTags) => res.json(updatedProductTags))
+    .then((updatedProductTags) => {
+      res.status(200).json(updatedProductTags)
+      console.log(updatedProductTags)
+    })
     .catch((err) => {
       // console.log(err);
       res.status(400).json(err);
     });
 });
 
-router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No location found with this id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
